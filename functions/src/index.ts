093ab3712024f282b8c379dffd73f2e94f07a352
Harrison Symes <symeshjb@gmail.com>
2023-04-07 16:50:01 +1200

    Changes to fix routing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
-import { https, logger } from "firebase-functions";
+import * as firebaseFunctions from "firebase-functions";
 
+import express from "express";
+import path from "path";
+
+const app = express();
+
+// Serve static files from the build folder
+app.use(express.static(path.join(__dirname, "build")));
+
+// Serve the index.html file for all routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+export const SPA = firebaseFunctions.https.onRequest(app);
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = https.onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+export const helloWorld = firebaseFunctions.https.onRequest(
+  (request, response) => {
+    firebaseFunctions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+  }
+);
