0fd08b3d8c1f83e41a4cb62b4ac4b60b249ce746
Harrison Symes <symeshjb@gmail.com>
2023-04-07 17:01:10 +1200

    Web routing fixed

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,20 @@
 import * as firebaseFunctions from "firebase-functions";
 
-import express from "express";
-import path from "path";
+// import express from "express";
+// import path from "path";
 
-const app = express();
+// const app = express();
 
-// Serve static files from the build folder
-app.use(express.static(path.join(__dirname, "build")));
+// // Serve static files from the build folder
+// app.use(express.static(path.join(__dirname, "build")));
 
-// Serve the index.html file for all routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// // Serve the index.html file for all routes
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
-export const SPA = firebaseFunctions.https.onRequest(app);
-// // Start writing functions
+// export const SPA = firebaseFunctions.https.onRequest(app);
+// // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = firebaseFunctions.https.onRequest(
